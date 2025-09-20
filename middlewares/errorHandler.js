b8013a9d12fac7533f5e5c1a044d081aad107fd1
Dev-Aaron27 <gamewithaaron270@gmail.com>
2025-09-20 11:54:44 +0100

    Create errorHandler.js

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,9 @@
+// middlewares/errorHandler.js
+export default function errorHandler(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong on the server",
+    error: err.message
+  });
+}
